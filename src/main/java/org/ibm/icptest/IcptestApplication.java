8add4c6e55c6170474040dff5730c641f3baf173
Mossaab KORAICHI <mosaab.koraichi@ibm.com>
2019-07-11 15:25:20 +0100

    first commit

---
+++ src/main/java/org/ibm/icptest/IcptestApplication.java
@@ -0,0 +1,13 @@
+package org.ibm.icptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcptestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcptestApplication.class, args);
+    }
+
+}
